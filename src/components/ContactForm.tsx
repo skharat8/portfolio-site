2847a7682d7d023ec48a67427cb8ead021bf98a6
skharat8 <5607079+skharat8@users.noreply.github.com>
2025-06-07 16:44:55 -0700

    Enable contact form to send messages
    - Add automatic subject line to the email
    - Fix bug with message error not being cleared due to ref usage
      conflicting with react hook form's internal ref
    - Improve spacing when errors are present
    - Change button on loading and message sent status

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,28 +1,55 @@
-import React from "react";
-import { type FieldValues, type SubmitHandler, useForm } from "react-hook-form";
+import React, { useImperativeHandle } from "react";
+import { useForm, useWatch } from "react-hook-form";
 
-import { Send, SendHorizontal } from "lucide-react";
+import { LoaderCircle, Send } from "lucide-react";
 
 import { cn, tw } from "@/lib/utils";
 
 type FormData = {
   name: string;
   email: string;
+  subject: string;
   message: string;
-  "access-key": string;
+  access_key: string;
 };
 
+enum FormStatus {
+  INITIAL = "initial",
+  SENDING = "sending",
+  SUCCESS = "success",
+  ERROR = "error",
+}
+
 function ContactForm() {
-  const [status, setStatus] = React.useState("");
+  const [status, setStatus] = React.useState<FormStatus>(FormStatus.INITIAL);
   const textAreaRef = React.useRef<HTMLTextAreaElement>(null);
 
   const {
     register,
     handleSubmit,
     reset,
+    clearErrors,
     subscribe,
+    setValue,
+    control,
     formState: { errors },
   } = useForm<FormData>();
+
+  const { ref, ...rest } = register("message", {
+    required: "Message cannot be empty",
+  });
+
+  useImperativeHandle(ref, () => textAreaRef.current);
+
+  const userName = useWatch({
+    control,
+    name: "name",
+    defaultValue: "Someone",
+  });
+
+  React.useEffect(() => {
+    setValue("subject", `Message from ${userName}`);
+  }, [userName]);
 
   React.useEffect(() => {
     const callback = subscribe({
@@ -35,32 +62,36 @@ function ContactForm() {
         }
       },
     });
-
     return () => callback();
   }, [subscribe]);
 
   async function onSubmit(formData: FormData) {
-    setStatus("sending");
+    setStatus(FormStatus.SENDING);
 
-    formData["access-key"] = "KEY";
+    formData["access_key"] = "4c2a6e40-8a80-4366-8cf7-8ba0ddaf36b8";
     const res = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
-      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData, null, 2),
     });
 
     const data = await res.json();
     if (data.success) {
-      setStatus("Form Submitted!");
+      setStatus(FormStatus.SUCCESS);
+      clearErrors();
       reset();
     } else {
+      setStatus(FormStatus.ERROR);
       console.error(data);
-      setStatus(data.message);
     }
   }
 
   const inputStyles = tw`outline-accent-red-600 h-6 rounded-sm bg-slate-400 px-2 text-slate-950
   placeholder:pt-1 focus-visible:outline-3`;
-  const errorStyles = "text-left text-sm font-bold text-accent-red-600 my-1";
+  const errorStyles = tw`text-accent-red-500 text-sm`;
   const labelStyles = tw`font-bold`;
 
   return (
@@ -68,20 +99,24 @@ function ContactForm() {
       className="mx-auto w-[min(50rem,95%)] rounded-md border-2 border-slate-500 bg-slate-800
         px-6 py-5 font-[Noto_Sans_Variable] text-slate-200"
     >
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className={`flex flex-col ${errors.email ? "gap-2" : "gap-4"}`}
+      >
         <h3 className="text-lg font-extrabold">Send Me a Message</h3>
         <p className="mb-1 text-slate-400 italic">
           Let's start a conversation. Fill out the form below and I'll get back
           to you as soon as possible.
         </p>
 
-        <div className="flex flex-wrap gap-4">
+        <div className={`flex flex-wrap ${errors.name ? "gap-2" : "gap-4"}`}>
           <div className="mr-auto flex flex-1 flex-col gap-2">
             <label className={labelStyles}>Name</label>
             <input
               {...register("name", { required: "Full name is required" })}
               type="text"
               placeholder="Your name"
+              disabled={status === FormStatus.SENDING}
               className={inputStyles}
             />
             {errors.name && (
@@ -91,10 +126,11 @@ function ContactForm() {
           <div className="flex flex-1 flex-col gap-2">
             <label className={labelStyles}>Email</label>
             <input
-              {...register("email", { required: "Invalid email" })}
+              {...register("email", { required: "Please enter a valid email" })}
               type="email"
               autoComplete="email"
               placeholder="Your email"
+              disabled={status === FormStatus.SENDING}
               className={inputStyles}
             />
             {errors.email && (
@@ -106,9 +142,10 @@ function ContactForm() {
         <div className="flex flex-col gap-2">
           <label className={labelStyles}>Message</label>
           <textarea
-            {...register("message", { required: "Message cannot be empty" })}
-            className={cn(inputStyles, "max-h-[30rem] min-h-10")}
+            {...rest}
+            className={cn(inputStyles, "max-h-[40rem] min-h-10")}
             placeholder="Write your message here"
+            disabled={status === FormStatus.SENDING}
             ref={textAreaRef}
           ></textarea>
           {errors.message && (
@@ -118,13 +155,25 @@ function ContactForm() {
 
         <button
           className="from-accent-red-500 to-accent-red-400 hover:from-accent-red-500/80
-            hover:to-accent-red-400/80 text-accent-red-50 mt-1 flex w-full items-center
+            hover:to-accent-red-400/80 text-accent-red-50 mt-2 flex w-full items-center
             justify-center gap-2 self-center rounded-sm bg-gradient-to-r py-2 pr-2 pl-4
             font-bold sm:w-[14rem]"
           type="submit"
+          disabled={status === FormStatus.SENDING}
         >
-          Send Message
-          <Send size={16} />
+          {status === FormStatus.SENDING && (
+            <>
+              Send Message
+              <LoaderCircle size={16} />
+            </>
+          )}
+          {status === FormStatus.SUCCESS && <span>Successfully Sent!</span>}
+          {(status === FormStatus.INITIAL || status === FormStatus.ERROR) && (
+            <>
+              Send Message
+              <Send size={16} />
+            </>
+          )}
         </button>
       </form>
     </div>
