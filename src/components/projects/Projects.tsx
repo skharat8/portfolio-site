00453b0622fa9052e25f8b6a191aa95c63e6f8ac
skharat8 <5607079+skharat8@users.noreply.github.com>
2025-06-20 00:32:50 -0700

    Move the profile firebird image to the top in desktop layout

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -23,23 +23,6 @@ function Projects() {
         <div className="mb-4 flex flex-col gap-7 lg:mt-6 lg:flex-row">
           <div className="flex flex-1 flex-col gap-4">
             <motion.div
-              className="group relative w-full overflow-hidden rounded-lg"
-              initial={{ opacity: 0 }}
-              viewport={{ once: true }}
-              whileInView={{ opacity: 1 }}
-              transition={{ delay: 0.2 }}
-            >
-              <picture>
-                <source type="image/webp" srcSet={firebirdHomeWebp} />
-                <img
-                  src={firebirdHomePng}
-                  className="origin-[top_left] rounded-lg"
-                  alt="Home page of the Firebird app"
-                />
-              </picture>
-            </motion.div>
-
-            <motion.div
               className="hidden h-[30vh] w-full overflow-hidden rounded-lg lg:block"
               initial={{ opacity: 0 }}
               viewport={{ once: true }}
@@ -52,6 +35,23 @@ function Projects() {
                   src={firebirdProfileJpg}
                   className="-translate-y-2 rounded-lg"
                   alt="Snapshot of the profile page of the Firebird app"
+                />
+              </picture>
+            </motion.div>
+
+            <motion.div
+              className="group relative w-full overflow-hidden rounded-lg"
+              initial={{ opacity: 0 }}
+              viewport={{ once: true }}
+              whileInView={{ opacity: 1 }}
+              transition={{ delay: 0.2 }}
+            >
+              <picture>
+                <source type="image/webp" srcSet={firebirdHomeWebp} />
+                <img
+                  src={firebirdHomePng}
+                  className="origin-[top_left] rounded-lg"
+                  alt="Home page of the Firebird app"
                 />
               </picture>
             </motion.div>
